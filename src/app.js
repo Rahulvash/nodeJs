11fbb8ed95b339cc31e924647640e796a6863abd
rahul.vashishtha <rahul.vashishtha@maxlifeinsurance.com>
2025-04-27 16:23:18 +0530

    api data saved in the database

--- src/app.js
+++ src/app.js
@@ -2,40 +2,51 @@ const express =  require('express');
 const app = express();
 const  { adminAuth }= require('./middleware/adminAuth.js');
 const { userAuth } = require('./middleware/userAuth.js');
+const connectDB = require('./config/database.js');
 
+const User = require('./models/user.js');
 
-app.use('/admin', adminAuth)
-
-app.get("/admin/getAllData",(req,res,next) => {
-    console.log("Admin getAllData Request 1st");
-    next();
-},(req,res) =>{
-    console.log('Admin getAllData Request 2nd');
-    res.send('Admin getAllData Request 2nd !!!!!!!!!!!');
-})
-
-app.get("/user/login",(req,res,next) => {
-    console.log("/user/login is called 1st");
-    res.send('/user/login is called 1st !!!!!!!!!!!');
-})
-
-app.get("/user/getAllData", userAuth, (req,res,next) => {
-    console.log("Get user getAllData Request 1st");
-    next();
-}, (req, res) => {
-    console.log('Get user getAllData Request 2nd');
-    res.send('Get user getAllData Request 2nd !!!!!!!!!!!');
-})
-
-
-// Error Handling Middleware
-app.use("/",(err, req, res, next) => {
-    if(err) {
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    }
+connectDB().then(() => {
+    console.log('MongoDB cluster connected successfully');
+    app.listen(7777, () => {
+        console.log('Server is running on port 7777');
+    });
+}).catch((err) => {
+    console.error('MongoDB cluster connection error:', err);
 });
 
-app.listen(7777, () => {
-    console.log('Server is running on port 7777');
+
+app.post("/signup",async (req, res) => {
+    const userObject1 = {
+        firstName : "Manu",
+        lastName : "Sharma",
+        userName : "manuSharma",
+        email : "manu@sharma.com",
+        password : "manuSharma",
+        phoneNumber : "9999999999",
+        age : 30,
+        gender: "Male"
+    }
+
+    // const userObject2 = {
+    //     firstName : req.body.firstName,
+    //     lastName : req.body.lastName,
+    //     userName : req.body.userName,
+    //     email : req.body.email,
+    //     password : req.body.password,
+    //     phoneNumber : req.body.phoneNumber,
+    //     age : req.body.age,
+    //     gender: req.body.gender
+    // }
+    
+   
+    try {
+        const user = new User(userObject1); // create a new user instance with the userObject1 data
+        await user.save(); // save the user instance to the database
+        console.log('User saved successfully:', user);
+        res.send('Signup successful');
+    } catch (error) {
+        console.log("Error saving user:", error);
+        res.status(500).send('Error saving user');
+    }
 });
