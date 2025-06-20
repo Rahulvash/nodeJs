faa7b00d2f75ca58f8b8f1e3a9bf93b225dd539e
rahul.vashishtha <rahul.vashishtha@maxlifeinsurance.com>
2025-06-21 00:07:51 +0530

    jwt token attached send from login api and created one middleware with user Auth what will verify the jwt token

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ const  { adminAuth }= require('./middleware/adminAuth.js');
 const { userAuth } = require('./middleware/userAuth.js');
 const connectDB = require('./config/database.js');
 const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
+var cookieParser = require('cookie-parser');
+var jwt = require('jsonwebtoken');
 
 const { validateSignUp } = require('./utils/validatorSignUp.js');
 
 const User = require('./models/user.js');
 
 app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cookieParser())
 
 connectDB().then(() => {
     console.log('MongoDB cluster connected successfully');
@@ -21,7 +24,7 @@ connectDB().then(() => {
 });
 
 
-app.post("/signup",async (req, res) => {
+app.post("/signup",userAuth,async (req, res) => {
     // get the user data from the request body
     // validate the user data using the validatorSignUp utility
     try{
@@ -62,30 +65,10 @@ app.post("/signup",async (req, res) => {
     }
 });
 
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body; // get the email and password from the request body
-    try{
-        if(!email || !password) {
-            throw new Error("Email is required"); // throw an error if email is not provided
-        }
-        const user = await User.findOne({email});
-        if(!user) {
-            throw new Error("Invalid Credentials"); // throw an error if user is not found
-        }
-        const isMatch =  bcrypt.compare(password, user.password);
-        if(!isMatch) {
-            throw new Error("Invalid Credentials"); // throw an error if password does not match
-        }
-        res.send('Login successful'); // send a success response if login is successful
-    }
-    catch (error) {
-            console.log("Error saving user:", error);
-            res.status(400).send(`Error saving user: ${error.message}`); // send a 400 Bad Request response if there was an error saving the user
-        }
-})
 
 
-app.delete("/delete",async (req, res) => {
+
+app.delete("/delete",userAuth, async (req, res) => {
     const userID = req.body.userID; // get the user ID from the request body
     try{
         const deletedUser = await User.findByIdAndDelete(userID); // find the user by ID and delete it
@@ -100,26 +83,8 @@ app.delete("/delete",async (req, res) => {
     }
 })
 
-// Updating user data with findOneAndUpdate
-// app.patch("/update",async (req, res) => {
-//     const emailId = req.body.emailId; // get the email ID from the request body
-//     try{
-//         const updatedUser = await User.findOneAndUpdate({email : emailId}, req.body, { new: true }); // find the user by email and update it with the new data
-//         if(updatedUser){
-//             res.send("Email: User updated successfully"); // send a success response if the user was updated
-//         }
-//         else{
-//             res.status(404).send("Email: User not found"); // send a 404 Not Found response if the user was not found
-//         }
-//     }
-//     catch (error) {
-//         res.status(500).send('Email: Error updating user'); // send a 500 Internal Server Error response if there was an error updating the user
-//         console.log("Error updating user:", error);
-//     }
 
-// })
-
-app.patch("/update",async (req,res) =>{
+app.patch("/update",userAuth,async (req,res) =>{
     
     const data = req.body; // get the user data from the request body
     try{
@@ -144,14 +109,13 @@ app.patch("/update",async (req,res) =>{
         res.status(500).send('Error updating user: ' + error.message); // send a 500 Internal Server Error response if there was an error updating the user
         console.log("Error updating user:", error);
     }
-})
+});
 
 
-
-//
 
 // sending all the users data
-app.get("/feed",async (req, res) => {
+app.get("/feed",userAuth,async (req, res) => {
+    
     try {
         const data = await User.find({}); // find all users in the database
         res.send(data) // send the user data as a JSON response with a 200 OK status
@@ -161,4 +125,48 @@ app.get("/feed",async (req, res) => {
         res.status(500).send('Error fetching users'); // send a 500 Internal Server Error response if there was an error fetching the users
     }
     
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body; // get the email and password from the request body
+    try{
+        if(!email || !password) {
+            throw new Error("Email is required"); // throw an error if email is not provided
+        }
+        const user = await User.findOne({email});
+        if(!user) {
+            throw new Error("Invalid Credentials"); // throw an error if user is not found
+        }
+        const isMatch =  bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            throw new Error("Invalid Credentials"); // throw an error if password does not match
+        }else{
+            var token = jwt.sign({ _id: user._id }, "DevTinder@123", { expiresIn: '1d' }); // generate a token with the user ID and a secret key, set to expire in 1 day
+            console.log("Token generated:", token)
+            res.cookie('token', token, {
+                secure: process.env.NODE_ENV === 'production', // set the cookie to be secure in production
+                maxAge: 24 * 60 * 60 * 1000 // set the cookie to expire in 1 day
+            });
+            res.send('Login successful'); // send a success response if login is successful
+        }
+    }
+    catch (error) {
+            console.log("Error saving user:", error);
+            res.status(400).send(`Error saving user: ${error.message}`); // send a 400 Bad Request response if there was an error saving the user
+        }
+})
+
+app.get("/profile", userAuth ,async (req, res) => {
+    // const userID = req.query.userID; // get the user ID from the query parameters
+    try {
+        const user = req.user;
+        if (user) {
+            res.send(user); // send the user data as a JSON response with a 200 OK status
+        } else {
+            res.status(404).send('User not found'); // send a 404 Not Found response if the user was not found
+        }
+    } catch (error) {
+        console.log("Error fetching user:", error);
+        res.status(500).send('Error fetching user'); // send a 500 Internal Server Error response if there was an error fetching the user
+    }
 })
--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,12 +1,30 @@
-const userAuth = (req, res, next) => {
-    let authToken = req.headers['authorization'];
-    let validToken = 'abcd';
-    if (!(authToken === validToken)) {
-        console.log('No Token Provided for userAuth');
-        return res.status(401).send('Unauthorized: No Token Provided');
+const jwt = require('jsonwebtoken'); // import the jsonwebtoken library
+const User = require('../models/user'); // import the User model
+
+const cookieParser = require('cookie-parser'); // import the cookie-parser middleware
+
+const userAuth = async (req, res, next) => {
+    console.log("req",req);
+    
+    const { token } = req.cookies; // get the token from the cookies
+    if (!token) {
+        return res.status(401).send('Unauthorized'); // send a 401 Unauthorized response if the token is not present
     }
-    next();
-}
+    try{
+        var decoded = await jwt.verify(token, 'DevTinder@123');
+        console.log("Decoded token:", decoded);
+        const user = await User.findById(decoded._id); // find the user by ID
+        if (!user) {
+            return res.status(404).send('User not found'); // send a 404 Not Found response if the user is not found
+        }
+        res.send(user);
+        next();
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        res.status(401).send('Unauthorized');
+    }
+    
+};
 
 module.exports = { userAuth };
 // Compare this snippet from src/app.js:
